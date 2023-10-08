ffc1c301e4165c868e432d7e5f5bb4adbdc98e16
SnackyTNK <142704870+SnackyTNK@users.noreply.github.com>
2023-10-08 18:13:50 +0700

    Add files via upload

---
+++ week_8/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+namespace Program
+{
+    class Finance
+    {
+        public static void Main(string[] args)
+        {
+            int number_product = 0;  //จำนวนสินค้า Loop
+            string product; //ประกาศตัวแปล ชื่อสินค้า
+            string price; //ประกาศตัวแปล ราคา
+
+            //Step 1 จำนวนสินค้า
+            Console.Write("How many product? : ");
+            number_product = Int32.Parse(Console.ReadLine()); //รับค่าคีบอร์ดเป็น String มาแปลงเป็นอินท์
+            string[][] data_product = new string[number_product][]; // aray
+
+            //Step 2 เก็บข้อมูลสินค้า
+            for (int i = 0; i < number_product; i++) // i เริ่มที่ 0 ถ้า i น้อยกว่าตัวน้อยรอบ จะทำซ้ำจนกว่าจะไม่ถึงตัวเลขที่กำหนด
+            {
+                Console.WriteLine();
+                Console.Write("input product name : ");     //ใส่ชื่อสินค้า
+                product = Console.ReadLine();
+
+                Console.Write("input price: ");            //ใส่ราคา
+                price = Console.ReadLine();
+
+                Console.WriteLine("--------------------");
+
+
+                data_product[i] = new string[] { product, price }; //รับค่าจากข้างบนมาเป็นข้อมูลไว้
+
+            }
+            double discount = 0.00;
+            double get = 0.00;
+            double Vats = 0.00;
+            double val = 0.00;
+
+            Console.WriteLine();
+            Console.Write("get the money? : ");
+            get = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Tax deducted (ex.3-10) ? : ");
+            Vats = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+            Console.Write("Discount(Bath) : ");
+            discount = Int32.Parse(Console.ReadLine());
+
+
+            val = (100 + Vats);
+
+
+            //แสดงผลออกมา
+            {
+                //head
+                Console.WriteLine("******************************************");
+                Console.WriteLine("                                          ");
+                Console.WriteLine("                   Bill                   ");
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("            Welcome to CarWash24          ");
+                Console.WriteLine("                                          ");
+                Console.WriteLine("#      Product                       price");
+                Console.WriteLine("------------------------------------------");
+
+                //Variable
+                double product_price = 0.00;
+                double vat = 0.00;
+                double Total = 0.00;
+                double change = 0.00;
+
+
+                //Step2
+                //Data Product
+                for (int i = 0; i < number_product; i++)
+                {
+                    Console.WriteLine
+                    (
+                        (i + 1) + "      " +
+                        data_product[i][0] + "                          " +
+                        data_product[i][1]
+                    );
+                    //ราคาสินค้าแต่ละรายการ
+                    Total += double.Parse(data_product[i][1]);
+                }
+                vat = Math.Round((Total * Vats) / val, 2);
+                Total = (Total - discount);
+                product_price = Total - vat;
+                change = (get - Total);
+
+                //Step 3
+                //footer
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("Product price                         " + product_price);
+                Console.WriteLine("Tax                                   " + vat);
+                Console.WriteLine("Discount                              " + discount);
+                Console.WriteLine("Total                                 " + Total);
+                Console.WriteLine();
+
+                Console.WriteLine("------------------------------------------");
+                Console.WriteLine("get the money                         " + get);
+                Console.WriteLine("change                                " + change);
+                Console.WriteLine("                                      ");
+                Console.WriteLine("     ******Thank You********          ");
+                Console.WriteLine("                                      ");
+                Console.WriteLine("                                      ");
+
+            }
+
+
+
+        }
+
+
+
+    }
+
+}
---
+++ week_9/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+namespace Program
+{
+    class CoffeeLuxer { }
+
+    class CoffeePremuim
+    {
+        public static void Main(string[] args) //จุดเริ่มต้นของการเริ่ม ( เหมือนประตูทางเข้า )
+        {
+            int val;
+
+            CoffeePremuim CP = new CoffeePremuim();//เปลี่ยนตัวแปลเป็นตัวย่อ
+            CP.selected_band(); //Display Band
+            val = Int32.Parse(Console.ReadLine());
+            CP.Line();
+            CP.Routes(val);
+            CP.Line();
+        }
+
+        public void selected_band()
+        {
+            Console.WriteLine("Please selected Band Coffee ");
+            Console.WriteLine("1 : Amezon ");
+            Console.WriteLine("2 : Inthanin");
+            Console.WriteLine("3 : Punthai");
+            Console.WriteLine("4 : D-Oro");
+            Console.Write("Selected : ");
+        }
+        public void Routes(int val)
+        {
+            CoffeePremuim CP = new CoffeePremuim();
+
+            switch (val)
+            {
+                case 1:
+                    {
+                        CP.amezon();
+                        break;
+                    }
+                case 2:
+                    {
+                        CP.inthanin();
+                        break;
+                    }
+                case 3:
+                    {
+                        CP.punthai();
+                        break;
+                    }
+                case 4:
+                    {
+                        CP.doro();
+                        break;
+                    }
+            }
+
+        }
+
+        public void Line()
+        {
+            Console.WriteLine("-----------------------");
+        }
+
+        public void amezon()
+        {
+            Console.WriteLine("******Amezon cafe******");
+            Console.WriteLine(" --What do you want-- ?");
+            Console.WriteLine(" 1. Black Coffee");
+            Console.WriteLine(" 2. Capu Chino");
+            Console.WriteLine(" 3. Late");
+        }
+
+        public void inthanin()
+        {
+            Console.WriteLine("*****Inthanin cafe*****");
+            Console.WriteLine(" --What do you want-- ?");
+            Console.WriteLine(" 1. Black Mix Orange");
+            Console.WriteLine(" 2. Capu Chino");
+            Console.WriteLine(" 3. Late");
+        }
+
+        public void punthai()
+        {
+            Console.WriteLine("******Punthai cafe*****");
+            Console.WriteLine(" --What do you want-- ?");
+            Console.WriteLine(" 1. Chocolate");
+            Console.WriteLine(" 2. Americano");
+        }
+
+        public void doro()
+        {
+            Console.WriteLine("******D - Oro cafe*****");
+            Console.WriteLine(" --What do you want-- ?");
+            Console.WriteLine(" 1. Milk Chocolate");
+            Console.WriteLine(" 2. Capu Chino");
+        }
+    }
+}
+
